55bf2ba26a9bf81728c1a8c0a096f574810a635a
NerD_JoKeR <kanjar_95@mail.ru>
2016-12-28 17:04:17 +0000

    добавил файлы проекта

---
+++ src/main/java/hello/model/Student.java
@@ -0,0 +1,74 @@
+package hello.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by JoKeR on 26.12.2016.
+ */
+public class Student implements Serializable {
+    private int id;
+    private int groupId;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String eMail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", eMail='" + eMail + '\'' +
+                '}';
+    }
+}
